0d33007476ae09383bfcb712f4ec2519dd478a8c
Md. Maruf Hossain <zzokofficial@gmail.com>
2026-06-23 05:22:59 +0600

    release OnlineBookStore v1.0.0.0

--- OnlineBookStore/Properties/AssemblyInfo.cs
+++ OnlineBookStore/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OxyOrb")]
 [assembly: AssemblyProduct("OnlineBookStore")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
-[assembly: AssemblyTrademark("zZOK")]
+[assembly: AssemblyCopyright("zZOK, ©  2026")]
+[assembly: AssemblyTrademark("Md. Maruf Hossain")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
